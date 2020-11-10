dad9705c5436c6e96bce32c9905c1050ffbd850b
Bruno <bruno@simicmail.com>
2020-11-10 11:04:40 +0100

    Update deploy_solution.sh

--- tigergraph/workshop/deploy_solution.sh
+++ tigergraph/workshop/deploy_solution.sh
@@ -2,14 +2,14 @@
 
 # check parameters
 if [[ ! $(which docker) && ! $(docker --version) ]]; then
-  echo "This workshop depends on Docker and docker-compose (ver. > 1.24 ). Please install them on your system."
+  echo "This workshop depends on Docker and docker-compose. Please install them on your system."
   echo "Howto install Docker: https://docs.docker.com/engine/install/"
   echo ".... and for compose: https://docs.docker.com/compose/install/"
   exit 2
 fi
 
 if [[ ! $(which docker-compose) && ! $(docker-compose --version)]]; then
-    echo "This workshop depends on Docker and docker-compose (ver. > 1.24 ). Please install them on your system."
+    echo "This workshop depends on Docker and docker-compose. Please install them on your system."
     echo "Howto install Docker: https://docs.docker.com/engine/install/"
     echo ".... and for compose: https://docs.docker.com/compose/install/"
     exit 2
